3a2431cd6f186d5bf26d7f7fdbad257a32b5c865
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2024-02-28 12:28:30 +1100

    Added particle effects to pickup

--- Pickups/Pickup.cs
+++ Pickups/Pickup.cs
@@ -2,13 +2,29 @@ using System;
 using Godot;
 
 public partial class Pickup : Node2D {
-	
+
 	// To compare against in player script
 	[Export]
 	public string pickupType = "test";
 
+	public PickupParticles particles;
+	public Sprite2D sprite;
+
+	public override void _Ready() {
+		// Load the particle effect
+		particles = GetNode<PickupParticles>("PickupParticles");
+		sprite = GetNode<Sprite2D>("Sprite2D");
+
+	}
+
 	public void Collect() {
-		// Builtin Godot method that queues the node for deletion
+		// Hide the texture so particles are visible
+		sprite.Visible = false;
+
+		// Spawn a set of particles
+		particles.Burst();
+
+		// Delete the object
 		QueueFree();
 	}
 }
