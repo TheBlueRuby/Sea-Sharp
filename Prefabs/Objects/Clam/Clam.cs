3e069938a3ee25d5786e56a653118466153211bd
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2024-03-05 23:47:31 +1100

    clam item is now changeable

--- Prefabs/Objects/Clam/Clam.cs
+++ Prefabs/Objects/Clam/Clam.cs
@@ -2,6 +2,7 @@ using System;
 using Godot;
 
 public partial class Clam : Node2D {
+	[Export]
 	private PackedScene clamPearl;
 	private Sprite2D texture;
 	private bool isPearlCollected;
@@ -10,7 +11,7 @@ public partial class Clam : Node2D {
 	private Texture2D openTexture;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
-		clamPearl = GD.Load<PackedScene>("res://Prefabs/Pickups/PickupBase.tscn");
+		clamPearl ??= GD.Load<PackedScene>("res://Prefabs/Pickups/PickupBase.tscn");
 		texture = GetNode<Sprite2D>("Sprite2D");
 		closedTexture = GD.Load<Texture2D>("res://Prefabs/Objects/Clam/clam.png");
 		openTexture = GD.Load<Texture2D>("res://Prefabs/Objects/Clam/clam_open.png");
