065702a471fd415e55a3ae50de317e94d9c7f1a6
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2024-03-08 16:20:17 +1100

    fixed warning in customelements.gd

--- Scripts/CustomElements.gd
+++ Scripts/CustomElements.gd
@@ -7,7 +7,7 @@ func _init() -> void:
 
 func draw_elevator(canvas_item: CanvasItem, coords: Vector3i, pos: Vector2, size: Vector2, _data: String):
 	# For elevator to display, both top and bottom cell must be discovered.
-	if not MetSys.is_cell_discovered(coords + Vector3i(0, -1, 0)) and not MetSys.is_cell_discovered(coords + Vector3i(0, size.y, 0)):
+	if not MetSys.is_cell_discovered(coords + Vector3i(0, -1, 0)) and not MetSys.is_cell_discovered(coords + Vector3i(0, int(size.y), 0)):
 		return
 	
 	var elevator_texture := preload("res://Sprites/Elevator.png")
