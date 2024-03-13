85e5ca229498823ef2c1691f50fa4af2b1bfbe82
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2024-03-13 19:39:34 +1100

    fixed pressure beam reading as obtained

--- Scripts/BitArray.cs
+++ Scripts/BitArray.cs
@@ -58,7 +58,7 @@ public class BitArray {
 
 	public string PrintArray() {
 		string output = "";
-		for (int i = 0; i < 32; i++) {
+		for (int i = 1; i <= 32; i++) {
 			output += CheckBit(i) ? "1" : "0";
 		}
 		return output;
