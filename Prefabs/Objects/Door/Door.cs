a1835302d05f5058c6c2a6b62550c7eb9ceb54d8
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2024-03-13 18:52:27 +1100

    added basic doors, replaced in most maps

--- Prefabs/Objects/Door/Door.cs
+++ Prefabs/Objects/Door/Door.cs
@@ -5,21 +5,62 @@ public partial class Door : Node2D {
 	private StaticBody2D doorCollision;
 	private Sprite2D doorSprite;
 
+	private const float doorOpenTime = 2f;
+	private float doorTimer = 0f;
+	private bool playerInArea;
+	private bool doorOpen;
+
+	[Export]
+	public string beamType;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
 		doorCollision = GetNode<StaticBody2D>("Collision");
 		doorSprite = GetNode<Sprite2D>("Collision/Sprite");
+
+		doorSprite.Texture = GD.Load<CompressedTexture2D>($"res://Prefabs/Objects/Door/{beamType}Door/{beamType}Door.png");
+	}
+
+	public override void _Process(double delta) {
+		if (!doorOpen) {
+			return;
+		}
+		if (doorTimer > 0f && playerInArea == false) {
+			doorTimer -= (float)delta;
+		} else if (!playerInArea) {
+			CloseDoor();
+
+		}
 	}
 
 	private void OnPlayerActivate(Node2D body) {
-		// Disable collision and hide door
-		doorCollision.ProcessMode = ProcessModeEnum.Disabled;
-		doorSprite.SetDeferred("visible", false);
+		playerInArea = true;
+		OpenDoor();
 	}
 
-	private async void OnPlayerDeactivate(Node2D body) {
-		// Enable collision and hide door after 2 seconds
-		await ToSignal(GetTree().CreateTimer(2), "timeout");
+	private void OnPlayerDeactivate(Node2D body) {
+		playerInArea = false;
+	}
+
+
+	private void OnBeamActivate(Node2D body) {
+		// If beam is the same type as the door or the door is a proximity door
+		if (body.SceneFilePath.Contains(beamType) || beamType == "Prox") {
+			OpenDoor();
+		}
+	}
+
+	public void OpenDoor() {
+		// GD.Print("Opening");
+		doorOpen = true;
+		doorCollision.ProcessMode = ProcessModeEnum.Disabled;
+		doorSprite.SetDeferred("visible", false);
+		doorTimer = doorOpenTime;
+	}
+
+	public void CloseDoor() {
+		// GD.Print("Closing");
+		doorOpen = false;
 		doorCollision.ProcessMode = ProcessModeEnum.Always;
 		doorSprite.SetDeferred("visible", true);
 	}
