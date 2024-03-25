dbfdec63bee8be89a63466fccc171044463ef22c
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2024-03-25 21:13:43 +1100

    hopefully fixed a race condition with pickups

--- Prefabs/Pickups/BeamPickup.cs
+++ Prefabs/Pickups/BeamPickup.cs
@@ -6,4 +6,14 @@ public partial class BeamPickup : Collectible {
 	// To compare against in player script
 	[Export]
 	public BeamTypes Type = BeamTypes.None;
+
+	private bool IsCollected = false;
+
+	public new void Collect() {
+		if (IsCollected) {
+			return;
+		}
+		IsCollected = true;
+		base.Collect();
+	}
 }
--- Prefabs/Pickups/ItemPickup.cs
+++ Prefabs/Pickups/ItemPickup.cs
@@ -6,4 +6,14 @@ public partial class ItemPickup : Collectible {
 	// To compare against in player script
 	[Export]
 	public ItemTypes Type = ItemTypes.Test;
+
+	private bool IsCollected = false;
+
+	public new void Collect() {
+		if (IsCollected) {
+			return;
+		}
+		IsCollected = true;
+		base.Collect();
+	}
 }
