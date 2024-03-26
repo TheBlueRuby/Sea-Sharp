f7c7c7dc1f21f33d600ffd3891947a692601559b
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2024-03-26 22:23:51 +1100

    added main menu

---
+++ Menus/MainMenu.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+using System.IO;
+
+public partial class MainMenu : Control
+{
+	private void OnPlayButtonPress()
+	{
+		GetTree().ChangeSceneToFile("res://GameLoop.tscn");
+	}
+
+
+	private void OnQuitButtonPress()
+	{
+		GetTree().Quit();
+	}
+
+	private void OnResetButtonPress()
+	{
+		if (Godot.FileAccess.FileExists("user://SeaSharpSave.sav")) {
+			DirAccess.Open("user://").Remove("SeaSharpSave.sav");
+		}
+	}
+}
