ebda1af57271a538721d650daad3122a8864d1a4
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2024-04-04 14:22:53 +1100

    text box now loads from json

--- Menus/PickupText.cs
+++ Menus/PickupText.cs
@@ -1,44 +1,99 @@
 using Godot;
 using System;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
 
 public partial class PickupText : Control {
 	[Export]
-	public string text = "";
-	[Export]
+	public string itemType = "AnglerCap";
+
 	public CompressedTexture2D itemIcon;
 
-	private bool textDone = false;
+	private const string itemList = "res://Prefabs/Pickups/ItemDescriptions.json";
+
+	private bool descDone = false;
+	private bool bindDone = false;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
-		itemIcon ??= GD.Load<CompressedTexture2D>("res://icon.svg");
+		itemIcon = GD.Load<CompressedTexture2D>(GetItemIcon(itemList));
 
 		GetNode<TextureRect>("Background/ItemIcon").Texture = itemIcon;
 
-		TextBuild();
+		PrintDesc();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta) {
-		if (!textDone) {
-			return;
-		}
-		if (Input.IsActionJustPressed("ui_accept")) {
-			QueueFree();
+		if (bindDone) {
+			if (Input.IsActionJustPressed("ui_accept")) {
+				QueueFree();
+			}
+
+		} else if (descDone) {
+			if (Input.IsActionJustPressed("ui_accept")) {
+				PrintBind();
+			}
 		}
 	}
 
-	private async void TextBuild() {
-		if (!textDone) {
+	private async void PrintDesc() {
+		descDone = await TextBuild(GetItemDesc(itemList), descDone);
+	}
+	private async void PrintBind() {
+		bindDone = await TextBuild(GetItemBind(itemList), bindDone);
+	}
+
+	private JsonNode GetItemFromJson(string file) {
+		string fileContents = FileAccess.Open(file, FileAccess.ModeFlags.Read).GetAsText();
+		JsonNode fileAsJson = JsonNode.Parse(fileContents);
+		JsonNode itemsArray = fileAsJson!["items"];
+		JsonNode item = null;
+
+		for (int i = 0; i < itemsArray.AsArray().Count; i++) {
+			if (itemsArray[i]!["id"]!.GetValue<string>() == itemType) {
+				item = itemsArray[i];
+			}
+		}
+		if (item != null) {
+			return item;
+		} else {
+			return null;
+		}
+
+	}
+
+
+	private string GetItemIcon(string file) {
+		return GetItemFromJson(file)!["icon"]!.GetValue<string>();
+	}
+	private string GetItemDesc(string file) {
+		return GetItemFromJson(file)!["desc"]!.GetValue<string>();
+	}
+	private string GetItemBind(string file) {
+		string bind = GetItemFromJson(file)!["bind"]!.GetValue<string>();
+
+		if (bind == "None") {
+			return "This item is used automatically.";
+		}
+
+		return $"Press ${bind} to use!";
+	}
+
+	private async Task<bool> TextBuild(string text, bool done) {
+		if (!done) {
+			GetNode<Label>("Background/Text").Text = "";
 			for (int i = 0; i < text.Length; i++) {
 				if (Input.IsActionPressed("ui_accept")) {
 					GetNode<Label>("Background/Text").Text = text;
 					break;
 				}
 				GetNode<Label>("Background/Text").Text += text[i];
+				GD.Print(text[i]);
 				await ToSignal(GetTree().CreateTimer(0.05F), "timeout");
 			}
-			await ToSignal(GetTree().CreateTimer(0.25F), "timeout");
-			textDone = true;
 		}
+		return true;
 	}
 }
