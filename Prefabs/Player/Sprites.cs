8abf4ff36e474a597468ad23ad0284d3cdb45537
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2024-04-13 15:15:07 +1000

    sprite scene should be working, unable to test until all sprites done

--- Prefabs/Player/Sprites.cs
+++ Prefabs/Player/Sprites.cs
@@ -1,15 +1,48 @@
 using Godot;
 using System;
 
-public partial class Sprites : Node2D
-{
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+namespace SeaSharp {
+	public partial class Sprites : Node2D {
+		private AnimatedSprite2D head;
+		private AnimatedSprite2D body;
+		private AnimatedSprite2D legs;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+		private string animation;
+
+		public string Animation {
+			get => animation;
+			set {
+				animation = value;
+				head.Animation = animation;
+				body.Animation = animation;
+				legs.Animation = animation;
+				if (animation == "Flipper") {
+					RotationDegrees = 90;
+				} else {
+					RotationDegrees = 0;
+				}
+			}
+		}
+		
+		public override void _Ready() {
+			head = GetNode<AnimatedSprite2D>("Head");
+			body = GetNode<AnimatedSprite2D>("Body");
+			legs = GetNode<AnimatedSprite2D>("Legs");
+		}
+
+		public void Play() {
+			head.Play();
+			body.Play();
+			legs.Play();
+		}
+		
+		public void Stop() {
+			head.Stop();
+			body.Stop();
+			legs.Stop();
+		}
+
+
+
 	}
 }
